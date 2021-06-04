c519f5196bd417756099acf3ac37cdb46a5ae36b
Hector <hsandoval79@gmail.com>
2021-06-04 04:23:33 -0700

    added multiplication and division methods with tests

--- test.py
+++ test.py
@@ -9,6 +9,12 @@ class TestCase(unittest.TestCase):
     def test_sub_1(self):
         self.assertEqual(example.subtraction(2, 1), 1)
 
+    def test_mul_1(self):
+        self.assertEqual(example.multiplication(2, 1), 2)
+
+    def test_div_1(self):
+        self.assertEqual(example.division(4, 2), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
